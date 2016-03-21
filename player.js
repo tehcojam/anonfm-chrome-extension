eab0364a8d3127e44c6e4a893b54d4e1ac43b45e
ekl-34 <ekl-34@yandex.ru>
2016-03-21 15:15:22 +0200

    crap

--- player.js
+++ player.js
@@ -20,66 +20,58 @@
 
 // });
 
-// fetch('http://anon.fm:8000/radio').then(function(r){
-// 	var reader = r.body.getReader();
-// 	console.log(reader)
-// 	return reader.read();
-// }).then(function(result, done) {
-// 	console.log(done);
-// 	console.log(result.done);
-//    	//var audioCtx = new AudioContext;
-	
-// 	function playRadio() {
-// //		if (!done) {
-// 			var tArray = result.value;
-// 			var buffer = new ArrayBuffer(tArray.length);
-// 			var view = new Uint8Array(buffer);
-
-// 			source = audioCtx.createBufferSource();
-// 			audioCtx.decodeAudioData(view.buffer, function(decodedData) {
-// 		        source.buffer = decodedData;
-// 		        source.connect(audioCtx.destination);
-// 		        source.start(0);
-// 		    });
-
-		    
-// //	    }
-// 		if(done) {console.log('ddddddddddddddone')}
-// 		//return playRadio();
-// 	}	
-
-// 	return playRadio();
-
-// }).catch(e => console.log(e))
-
-
-
 fetch('http://anon.fm:8000/radio').then(function(response) {
 	var reader = response.body.getReader();
 	var audioCtx = new AudioContext();
 	//var source = audioCtx.createBufferSource();
+
+	var buff = new Uint8Array(0);
+
+	function addToBuffer(chunk) {
+		var tArray = chunk.value;
+		var buffer = new ArrayBuffer(tArray.length);
+		var view = new Uint8Array(buffer);
+		view.set(tArray);
+		buff = arrayConcat(buff, view);
+
+		if (buff.length < 5000000) {
+			return reader.read().then(addToBuffer).catch(e => console.log(e));
+		}
+		return 'done';
+		
+	}
+
+
+	function arrayConcat(arr1, arr2) {
+		var resultArray = new Uint8Array(arr1.length + arr2.length);
+		resultArray.set(arr1);
+		resultArray.set(arr2, arr1.length);
+
+		return resultArray;
+	}
+
+	function playSample(done) {
+		var source = audioCtx.createBufferSource();
+		audioCtx.decodeAudioData(buff.buffer).then(function(decodedData) {
+	        source.buffer = decodedData;
+	        source.connect(audioCtx.destination);
+	        source.start(0);
+	        source.onended = function() {
+	        	console.log('onended++++++++++++++++++++++++++++++++++=');
+	        	buff = new Uint8Array(0);
+	        	playRadio();	
+	        }
+		})
+	}
+
+
 	function playRadio() {
-		return reader.read().then(function(result) {
-			if (!result.done) {
-				console.log(result.value)
-				var tArray = result.value;
-				var buffer = new ArrayBuffer(tArray.length);
-				var view = new Uint8Array(buffer);
-				view.set(tArray);
-
-				var source = audioCtx.createBufferSource();
-				audioCtx.decodeAudioData(view.buffer).then(function(decodedData) {
-			        source.buffer = decodedData;
-			        source.connect(audioCtx.destination);
-			        source.start(0);
-			    }).catch(e => console.log(e));
-			} else { console.log('ddddddddddddddone')}
-
-			//return playRadio();
-		}).catch(e => console.log(e));
-
+		return reader.read().then(addToBuffer).then(playSample).catch(e => console.log(e));
 	}
 
 
 	return playRadio();
 }).catch(e => console.log(e))
+
+AudioContext.createMediaElementSource()
+
